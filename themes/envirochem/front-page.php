4ef654be6b331871850bb35a4047ca1d9b17b0fd
njohnstud <n.e.johnston@hotmail.com>
2017-05-10 19:14:53 -0700

    Added hero image

--- themes/envirochem/front-page.php
+++ themes/envirochem/front-page.php
@@ -1,5 +1,13 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying the front-page.
+ *
+ * @package envirochem
+ */
+
+get_header(); ?>
 <section>
     <div class="hero-image">
+    hello
     </div>
 </section>
