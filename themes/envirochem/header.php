6ae1d77e2ade13471c1432dc3a7e50f383bb5c91
njohnstud <n.e.johnston@hotmail.com>
2017-06-05 19:12:00 -0700

    Added font Titillium Web

--- themes/envirochem/header.php
+++ themes/envirochem/header.php
@@ -8,21 +8,28 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<script src="https://use.fontawesome.com/c0922c38ac.js"></script>
+		<meta charset='<?php bloginfo( 'charset' ); ?>'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<link rel='profile' href='http://gmpg.org/xfn/11'>
+		<link rel='pingback' href='<?php bloginfo( 'pingback_url' ); ?>'>
+		<link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet'>
+		<script src='https://use.fontawesome.com/c0922c38ac.js'></script>
 	<?php wp_head(); ?>
 	</head>
 
 	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
+		<div id='page' class='hfeed site'>
+			<a class='skip-link screen-reader-text' href='#content'><?php esc_html( 'Skip to content' ); ?></a>
 
-			<header id="masthead" class="site-header" role="banner">
-				<span>E</span><i class="fa fa-tree"></i><span>C</span>
-			<? wp_nav_menu() ?>
+			<header id='masthead' class='site-header' role='banner'>
+				<div class='header-wrapper'>
+					<div class='logo'>
+						<span>E</span><i class='fa fa-tree'></i><span>C</span>
+					</div>
+					<div class='menu-wrapper'>
+						<? wp_nav_menu() ?>
+					</div>
+				</div>
 			</header><!-- #masthead -->
 
-			<div id="content" class="site-content">
+			<div id='content' class='site-content'>
