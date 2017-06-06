d452f3c334510be8bf359b0437357023ca81eb8d
njohnstud <n.e.johnston@hotmail.com>
2017-06-05 18:01:30 -0700

    Added tree header icon

--- themes/envirochem/header.php
+++ themes/envirochem/header.php
@@ -12,7 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+		<script src="https://use.fontawesome.com/c0922c38ac.js"></script>
 	<?php wp_head(); ?>
 	</head>
 
@@ -21,10 +21,7 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-			<div class="logo">
-				<h1 class="logo-part-enviro">enviro</h1>
-				<h1 class="logo-part-chem">chem</h1>
-			</div>
+				<i class="fa fa-tree"></i>
 			<? wp_nav_menu() ?>
 			</header><!-- #masthead -->
 
