ad14c608748b7218b385d40b02cf6523fef92ecc
njohnstud <n.e.johnston@hotmail.com>
2017-06-17 19:56:06 -0700

    Added reset

--- themes/envirochem/front-page.php
+++ themes/envirochem/front-page.php
@@ -8,6 +8,9 @@
 get_header(); ?>
 <section class="hero-wrapper">
     <div class="hero-image">
+    <div class='centered-logo-wrapper'>
+    <p class='centered-logo'>EnviroChem</p>
+    </div>
     </div>
 </section>
 <?php get_footer(); ?>
