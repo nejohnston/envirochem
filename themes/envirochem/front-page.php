207d8fbd94338ac46a7ae19180e1edc9812c5543
njohnstud <n.e.johnston@hotmail.com>
2017-06-17 23:01:34 -0700

    Centered landing page logo

--- themes/envirochem/front-page.php
+++ themes/envirochem/front-page.php
@@ -8,9 +8,14 @@
 get_header(); ?>
 <section class="hero-wrapper">
     <div class="hero-image">
-    <div class='centered-logo-wrapper'>
-    <div class='centered-logo'><span class='logo-top-text'>EnviroChem</span><span class='logo-bottom-text'>S o l u t i o n s</span></div>
-    </div>
+        <div class='centered-logo-wrapper'>
+            <div class='centered-logo'>
+                <div class='logo-inner-wrapper'>
+                    <h1 class='logo-top-text'>EnviroChem</h1>
+                    <h2 class='logo-bottom-text'>S o l u t i o n s</h1>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 <?php get_footer(); ?>
