5ff03c07910b6a8a97371556ab4bc587e067497b
njohnstud <n.e.johnston@hotmail.com>
2017-06-19 22:19:44 -0700

    Added logo to footer

--- themes/envirochem/footer.php
+++ themes/envirochem/footer.php
@@ -11,19 +11,23 @@
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="align-center footer-info">
 				<div class="footer-block-item">
-					<h1 class="footer-header">Contact Info</h3>
+					<h1 class="footer-header">Contact Info</h1>
 					<p><i class="fa fa-envelope"></i><a class="footer-link" href="mailto: response@envirochem.com">response@envirochem.com</a></p>
 					<p><i class="fa fa-phone"></i><a class="footer-link" href="tel:6049860233">604-986-0233</a></p>
 					<span><i class="fa fa-twitter"></i><i class="fa fa-facebook"></i><i class="fa fa-pinterest"></i></span>
 					</div>
 					<div class="footer-block-item">
-					<h1 class="footer-header">Business Hours</h3>
-					<p>Monday to Friday: 9am to 5pm</p>
-					<p>Weekends: Closed</p>
+					<h1 class="footer-header">Business Hours</h1>
+						<p>Monday to Friday: 9am to 5pm</p>
+						<p>Weekends: Closed</p>
 					</div>
-					<div class="footer-block-item">
-						<div class="text-logo">
-					</div>
+					<div class="text-logo">
+						<div class="centered-logo">
+							<div class='logo-inner-wrapper'>
+                    			<h1 class='logo-top-text'>EnviroChem</h1>
+                    			<h2 class='logo-bottom-text'>S o l u t i o n s</h1>
+  							</div>
+						</div>
 					</div>
 			</div><!-- .site-info -->
 			<div class="copyright">
