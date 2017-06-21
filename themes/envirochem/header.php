d7c21a229b384baa81707d158dc537f1add9165e
njohnstud <n.e.johnston@hotmail.com>
2017-06-20 17:02:43 -0700

    added pure css menu

--- themes/envirochem/header.php
+++ themes/envirochem/header.php
@@ -25,10 +25,37 @@
 					<div class='logo'>
 						<span>E</span><i class='fa fa-tree'></i><span>C</span>
 					</div>
-					<div class='menu-wrapper'>
+<!--    Made by Erik Terwan    -->
+<!--   24th of November 2015   -->
+<!--    All rights reserved    -->
+<nav role="navigation">
+  <div id="menuToggle">
+    <!--
+    A fake / hidden checkbox is used as click reciever,
+    so you can use the :checked selector on it.
+    -->
+    <input type="checkbox" />
+
+    <!--
+    Some spans to act as a hamburger.
+
+    They are acting like a real hamburger,
+    not that McDonalds stuff.
+    -->
+    <span></span>
+    <span></span>
+    <span></span>
+
+    <!--
+    Too bad the menu has to be inside of the button
+    but hey, it's pure CSS magic.
+    -->
+    <ul id="menu">
 						<? wp_nav_menu() ?>
-					</div>
-				</div>
+
+    </ul>
+  </div>
+</nav>
 			</div>
 		</header><!-- #masthead -->
 
--- themes/envirochem/js/navigation.js
+++ themes/envirochem/js/navigation.js
@@ -1,13 +1,14 @@
+
 (function($) {
 $('#toggle').toggle(
     function() {
         $('#popout').animate({ left: 0 }, 'slow', function() {
-            $('#toggle').html('<img src="http://www.example.com/wp-content/themes/your-theme/images/menu.png" alt="close" />');
+            $('#toggle').html('<img src="http://localhost:8888/envirochem/assets/images/menu.png" alt="Navigation menu icon" />');
         });
     },
     function() {
         $('#popout').animate({ left: -250 }, 'slow', function() {
-            $('#toggle').html('<img src="http://www.example.com/wp-content/themes/your-theme/images/menu.png" alt="close" />');
+            $('#toggle').html('<img src="http://localhost:8888/envirochem/assets/images/menu.png" alt="Navigation menu icon" />');
         });
     }
 );
